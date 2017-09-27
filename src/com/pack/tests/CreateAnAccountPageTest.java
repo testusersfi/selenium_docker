89bacfe86596dfe6227cb5603aa67d126a9df8a1
srinivas.bavirisetti <srinivas.bavirisetti@accenture.com>
2017-09-27 23:44:37 +0530

    latest selenium sample code

---
+++ src/com/pack/tests/CreateAnAccountPageTest.java
@@ -0,0 +1,39 @@
+package com.pack.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.pack.base.TestBaseSetup;
+
+import com.pack.pageobjects.BasePage;
+import com.pack.pageobjects.CreateAccountPage;
+import com.pack.pageobjects.SignInPage;
+
+public class CreateAnAccountPageTest extends TestBaseSetup {
+	private WebDriver driver;
+	private SignInPage signInPage;
+	private BasePage basePage;
+	private CreateAccountPage createAccountPage;
+
+	@BeforeClass
+	public void setUp() {
+		driver = getDriver();
+	}
+
+	@Test
+	public void verifyCreateAnAccounPage() {
+		System.out.println("Create An Account page test...");
+		basePage = new BasePage(driver);
+		signInPage = basePage.clickSignInBtn();
+		createAccountPage = signInPage.clickonCreateAnAccount();
+		Assert.assertTrue(createAccountPage.verifyPageTitle(), "Page title not matching");
+		Assert.assertTrue(createAccountPage.verifyCreateAccountPageText(), "Page text not matching");
+	}
+
+	public void verifySignInFunction() {
+
+	}
+
+}
